8c93977c13a46ce63b5921204923edec6665d9e5
Aaron-Wright <georonathan47@gmail.com>
2020-07-15 17:36:10 +0000

    troublw

--- NumberTrivia/lib/FEATURES/Domain/UseCases/getconcretenumbertrivia.dart
+++ NumberTrivia/lib/FEATURES/Domain/UseCases/getconcretenumbertrivia.dart
@@ -1,3 +1,5 @@
 class GetConcreteNumberTrivia {
-  final NumberTriviaRepository repository;
+  final NumberTriviaRepository repository;// this line doesn't allow me to pass it
+  //into a constructor and doesn't allow me to import the other packages
+  
 }
