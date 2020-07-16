57b5f3c43d91611d8131cbb484eeb5b499870233
Aaron-Wright <georonathan47@gmail.com>
2020-07-16 10:25:24 +0000

    failed tests

--- NumberTrivia/lib/FEATURES/Domain/UseCases/getconcretenumbertrivia.dart
+++ NumberTrivia/lib/FEATURES/Domain/UseCases/getconcretenumbertrivia.dart
@@ -1,3 +1,10 @@
+import 'package:NumberTrivia/CORE/Errors/Failures.dart';
+import 'package:NumberTrivia/FEATURES/Domain/Entities/Number_Trivia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
 import '../Repositories/Number_Trivia_repositories.dart';
 
 class GetConcreteNumberTrivia {
@@ -5,7 +12,18 @@ class GetConcreteNumberTrivia {
 
   GetConcreteNumberTrivia(this.repository);
 
-  //execute({int number}) {} // this line doesn't allow me to pass it
-  //into a constructor and doesn't allow me to import the other packages
+  execute({int number}) async {}
 
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
+  }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number});
+
+  @override
+  List<Object> get props => [number];
 }
