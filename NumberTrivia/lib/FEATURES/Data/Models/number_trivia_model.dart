49c9267a79d02f31c01cedaf89438ab38ccc60e3
Aaron-Wright <georonathan47@gmail.com>
2020-07-18 22:23:15 +0000

    Successfully implemented both JSON methods

---
+++ NumberTrivia/lib/FEATURES/Data/Models/number_trivia_model.dart
@@ -0,0 +1,24 @@
+import 'package:meta/meta.dart';
+
+import '../../Domain/Entities/Number_Trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
