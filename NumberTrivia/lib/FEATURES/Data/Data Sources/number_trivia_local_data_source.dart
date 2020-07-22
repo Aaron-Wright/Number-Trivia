c167bd8c4df45f5bfaab7f6fafe48ceef1834ea6
Aaron-Wright <georonathan47@gmail.com>
2020-07-22 16:18:04 +0000

    local data sources problem

--- NumberTrivia/lib/FEATURES/Data/Data Sources/number_trivia_local_data_source.dart
+++ NumberTrivia/lib/FEATURES/Data/Data Sources/number_trivia_local_data_source.dart
@@ -1,5 +1,8 @@
-import '';
-
 abstract class NumberTriviaLocalDataSource {
+  ///Gets the cached [NumberTriviaModel] which was gotten last time
+  ///the user had an internet connection
+  ///
+  ///Throws [NoLocalException] if no cached data is present
   Future<NumberTriviaModel> getLastNumberTrivia();
-}
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+} //why can't import NumberTriviaModel???
