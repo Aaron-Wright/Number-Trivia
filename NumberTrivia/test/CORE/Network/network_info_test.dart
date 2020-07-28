2cb45329c4c01447ba3ef3e45b402bd1f8b686fe
Aaron-Wright <georonathan47@gmail.com>
2020-07-28 08:45:33 +0000

    data connection checker package

--- NumberTrivia/test/CORE/Network/network_info_test.dart
+++ NumberTrivia/test/CORE/Network/network_info_test.dart
@@ -2,7 +2,7 @@ import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConncetionChecker {}
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
 
 void main() {
   NetworkInfoImpl networkInfoImpl;
